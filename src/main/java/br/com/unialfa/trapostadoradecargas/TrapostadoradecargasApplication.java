50f5bd7fbd867e307157e0bbb595113d93b980d3
JoaoMarcosFalcao <j.marcos8616@gmail.com>
2021-04-02 21:30:46 -0400

    First

---
+++ src/main/java/br/com/unialfa/trapostadoradecargas/TrapostadoradecargasApplication.java
@@ -0,0 +1,13 @@
+package br.com.unialfa.trapostadoradecargas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrapostadoradecargasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrapostadoradecargasApplication.class, args);
+	}
+
+}
